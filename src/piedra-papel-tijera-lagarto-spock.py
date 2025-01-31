c42b7dfab422e91013e33695e412d5b9aa086d66
Valwraek <bardiarajabirajabi@gmail.com>
2025-01-31 14:15:20 +0100

    feat(src): :sparkles: add new function assess_game

--- src/piedra-papel-tijera-lagarto-spock.py
+++ src/piedra-papel-tijera-lagarto-spock.py
@@ -1,10 +1,10 @@
 from enum import IntEnum
 
 class Game_action(IntEnum):
-    PIEDRA = 0
-    PAPEL = 1
-    TIJERA = 2
-    LAGARTO = 3
+    ROCK = 0
+    PAPER = 1
+    SCISSORS = 2
+    LIZARD = 3
     SPOCK = 4
 
 class Game_result(IntEnum):
@@ -15,10 +15,16 @@ class Game_result(IntEnum):
 class Juego:
     def __init__(self):
         self.victorias = {
-            Game_action.PIEDRA: [Game_action.TIJERA, Game_action.LAGARTO],
-            Game_action.PAPEL: [Game_action.PIEDRA, Game_action.SPOCK],
-            Game_action.TIJERA: [Game_action.PAPEL, Game_action.LAGARTO],
-            Game_action.LAGARTO: [Game_action.SPOCK, Game_action.PAPEL],
-            Game_action.SPOCK: [Game_action.PIEDRA, Game_action.TIJERA]
+            Game_action.ROCK: [Game_action.SCISSORS, Game_action.LIZARD],
+            Game_action.PAPER: [Game_action.ROCK, Game_action.SPOCK],
+            Game_action.SCISSORS: [Game_action.PAPER, Game_action.LIZARD],
+            Game_action.LIZARD: [Game_action.SPOCK, Game_action.PAPER],
+            Game_action.SPOCK: [Game_action.ROCK, Game_action.SCISSORS]
         }
+    def assess_game(self, user_action, computer_action):
+        if user_action == computer_action:
+            return Game_result.EMPATE
+        if computer_action in self.victorias[user_action]:
+            return Game_result.VICTORIA
+        return Game_result.DERROTA
     
