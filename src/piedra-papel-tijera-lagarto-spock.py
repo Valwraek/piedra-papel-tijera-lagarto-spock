5176483719624485fae4f53b71e999398f234d51
Valwraek <bardiarajabirajabi@gmail.com>
2025-01-31 14:06:56 +0100

    feat(src): :sparkles: add constructor

--- src/piedra-papel-tijera-lagarto-spock.py
+++ src/piedra-papel-tijera-lagarto-spock.py
@@ -12,3 +12,13 @@ class Game_result(IntEnum):
     VICTORIA = 1
     EMPATE = 2
 
+class Juego:
+    def __init__(self):
+        self.victorias = {
+            Game_action.PIEDRA: [Game_action.TIJERA, Game_action.LAGARTO],
+            Game_action.PAPEL: [Game_action.PIEDRA, Game_action.SPOCK],
+            Game_action.TIJERA: [Game_action.PAPEL, Game_action.LAGARTO],
+            Game_action.LAGARTO: [Game_action.SPOCK, Game_action.PAPEL],
+            Game_action.SPOCK: [Game_action.PIEDRA, Game_action.TIJERA]
+        }
+    
